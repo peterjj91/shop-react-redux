912a21eda9a261341186cd274a4bcc4f93f09ca0
Pavel Popeskul <peterjj91@gmail.com>
2020-02-04 01:13:57 +0200

    Catalog + item + cart

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import "./main.css";
+import './main.css'
 
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore, applyMiddleware } from "redux";
-import { createBrowserHistory } from "history";
-import thunk from "redux-thunk";
-import { routerMiddleware, ConnectedRouter } from "connected-react-router";
-import { composeWithDevTools } from "redux-devtools-extension";
-import { Provider } from "react-redux";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {createBrowserHistory} from 'history'
+import thunk from 'redux-thunk'
+import {routerMiddleware, ConnectedRouter} from 'connected-react-router'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import {Provider} from 'react-redux'
 
-import createRootReducer from "reducers";
-import Layout from "containers/layout";
+import createRootReducer from 'reducers'
+import routes from 'routes'
 
-const history = createBrowserHistory();
-const middlewares = [thunk, routerMiddleware(history)];
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
 const store = createStore(
   createRootReducer(history),
   composeWithDevTools(applyMiddleware(...middlewares))
-);
+)
 
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Layout />
+      {routes}
     </ConnectedRouter>
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
